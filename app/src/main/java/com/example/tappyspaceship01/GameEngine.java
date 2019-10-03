f7f8f5f8f5a28e000d2189e1f4e23aecafb39356
simran bal <simranbal01@gmail.com>
2019-10-03 13:55:10 -0400

    added images

--- app/src/main/java/com/example/tappyspaceship01/GameEngine.java
+++ app/src/main/java/com/example/tappyspaceship01/GameEngine.java
@@ -18,10 +18,10 @@ import java.util.Random;
 public class GameEngine extends SurfaceView implements Runnable {
 
     // Android debug variables
-    final static String TAG="DINO-RAINBOWS";
+    final static String TAG = "DINO-RAINBOWS";
 
-    //900 pixels visible on screen and 100 pixels for 4 road rectangles
-    final static int LANE_HEIGHT= 200;
+
+    final static int LANE_HEIGHT = 200;
 
     // screen size
     int screenHeight;
@@ -40,11 +40,10 @@ public class GameEngine extends SurfaceView implements Runnable {
     Paint paintbrush;
 
 
-
     // -----------------------------------
     // GAME SPECIFIC VARIABLES
     // -----------------------------------
-    int lanes =4;
+    int lanes = 4;
     int laneTop;
     int laneBottom;
 
@@ -52,8 +51,12 @@ public class GameEngine extends SurfaceView implements Runnable {
     // ## SPRITES
     // ----------------------------
 
-     Rect laneRectangle;
+    Rect laneRectangle;
     Player dinosour;
+    Object candy;
+    Player rainbow;
+    Player garbage;
+    int dinoLane;
     // represent the TOP LEFT CORNER OF THE GRAPHIC
 
     // ----------------------------
@@ -79,81 +82,84 @@ public class GameEngine extends SurfaceView implements Runnable {
     }
 
 
-
     private void printScreenInfo() {
 
         Log.d(TAG, "Screen (w, h) = " + this.screenWidth + "," + this.screenHeight);
     }
 
-    private void spawnPlayer()
-    {
+    private void spawnPlayer() {
         Log.d(TAG, "spawnPlayer: Marks the beginning of SpawnPlayer");
         //@TODO: Start the player at the RIGHT side of screen
         //Initializing the player at 85% width as x and middle of screen as Y
-        dinosour = new Player(this.getContext(), (int)(screenWidth*.85),(int)(screenHeight*.5)-200);
-        //Initializing the 4 lanes
+        dinosour = new Player(this.getContext(), (int) (screenWidth * .85), (int) (screenHeight * .5) - 200);
+
+        candy = new Object(this.getContext(), (int) (screenWidth * 0.5), (int) (screenHeight * .5) - 550);
+
+        rainbow = new Player(this.getContext(), (int) (screenWidth * 0.15), (int) (screenHeight * 0.5) - 500);
 
     }
 
-    private void drawLaneOne()
-    {
+    private void drawLaneOne() {
         Log.d(TAG, "drawLaneFirst: ");
         paintbrush.setColor(Color.RED);
         paintbrush.setStyle(Paint.Style.FILL);
-        this.laneTop =LANE_HEIGHT-12;
-        this.laneBottom = LANE_HEIGHT+12;
-        Log.d(TAG, "drawLaneFirst: LaneTOP: "+laneTop);
-        Log.d(TAG, "drawLaneFirst: LaneBOTTOM: "+laneBottom);
+        this.laneTop = LANE_HEIGHT - 12;
+        this.laneBottom = LANE_HEIGHT + 12;
+        Log.d(TAG, "drawLaneFirst: LaneTOP: " + laneTop);
+        Log.d(TAG, "drawLaneFirst: LaneBOTTOM: " + laneBottom);
         this.laneRectangle = new Rect(50,
                 laneTop,
                 1400,
                 laneBottom);
-        this.canvas.drawRect(laneRectangle,paintbrush);
+        this.canvas.drawRect(laneRectangle, paintbrush);
     }
-    private void drawLaneTwo(){
+
+    private void drawLaneTwo() {
         Log.d(TAG, "drawLaneSecond: ");
         paintbrush.setColor(Color.RED);
         paintbrush.setStyle(Paint.Style.FILL);
-        this.laneTop =((LANE_HEIGHT)*2)-12;
-        this.laneBottom = ((LANE_HEIGHT)*2)+12;
-        Log.d(TAG, "drawLaneSecond: LaneTOP:"+laneTop);
-        Log.d(TAG, "drawLaneSecond: LaneBOTTOM:"+laneBottom);
+        this.laneTop = ((LANE_HEIGHT) * 2) - 12;
+        this.laneBottom = ((LANE_HEIGHT) * 2) + 12;
+        Log.d(TAG, "drawLaneSecond: LaneTOP:" + laneTop);
+        Log.d(TAG, "drawLaneSecond: LaneBOTTOM:" + laneBottom);
         this.laneRectangle = new Rect(50,
                 laneTop,
                 1400,
                 laneBottom);
-        this.canvas.drawRect(laneRectangle,paintbrush);
+        this.canvas.drawRect(laneRectangle, paintbrush);
 
     }
-    private void drawLaneThree(){
+
+    private void drawLaneThree() {
         Log.d(TAG, "drawLaneThird: ");
         paintbrush.setColor(Color.RED);
         paintbrush.setStyle(Paint.Style.FILL);
-        this.laneTop =((LANE_HEIGHT)*3)-12;
-        this.laneBottom = ((LANE_HEIGHT)*3)+12;
-        Log.d(TAG, "drawLaneThird: LaneTOP:"+laneTop);
-        Log.d(TAG, "drawLaneThird: LaneBOTTOM:"+laneBottom);
+        this.laneTop = ((LANE_HEIGHT) * 3) - 12;
+        this.laneBottom = ((LANE_HEIGHT) * 3) + 12;
+        Log.d(TAG, "drawLaneThird: LaneTOP:" + laneTop);
+        Log.d(TAG, "drawLaneThird: LaneBOTTOM:" + laneBottom);
 
         laneRectangle = new Rect(50,
                 laneTop,
                 1400,
                 laneBottom);
-        this.canvas.drawRect(laneRectangle,paintbrush);
+        this.canvas.drawRect(laneRectangle, paintbrush);
     }
-    private void drawLaneFour(){
+
+    private void drawLaneFour() {
         Log.d(TAG, "drawLaneFourth: ");
         paintbrush.setColor(Color.RED);
         paintbrush.setStyle(Paint.Style.FILL);
-        this.laneTop =((LANE_HEIGHT)*4)-12;
-        this.laneBottom = ((LANE_HEIGHT)*4)+12;
-        Log.d(TAG, "drawLaneFourth: LaneTOP:"+laneTop);
-        Log.d(TAG, "drawLaneFourth: LaneBOTTOM:"+laneBottom);
+        this.laneTop = ((LANE_HEIGHT) * 4) - 12;
+        this.laneBottom = ((LANE_HEIGHT) * 4) + 12;
+        Log.d(TAG, "drawLaneFourth: LaneTOP:" + laneTop);
+        Log.d(TAG, "drawLaneFourth: LaneBOTTOM:" + laneBottom);
 
         laneRectangle = new Rect(50,
                 laneTop,
                 1400,
                 laneBottom);
-        this.canvas.drawRect(laneRectangle,paintbrush);
+        this.canvas.drawRect(laneRectangle, paintbrush);
     }
 
 
@@ -197,10 +203,15 @@ public class GameEngine extends SurfaceView implements Runnable {
     // - update, draw, setFPS
     // ------------------------------
 
+    //using switch case for movement
     public void updatePositions() {
+
+
+
     }
 
-    public void redrawSprites() {
+    public void redrawSprites()
+    {
         Log.d(TAG, "redrawSprites: Marks the beginning of RedrawSprites");
         if (this.holder.getSurface().isValid()) {
             this.canvas = this.holder.lockCanvas();
@@ -208,7 +219,7 @@ public class GameEngine extends SurfaceView implements Runnable {
             //----------------
 
             // configure the drawing tools
-            this.canvas.drawColor(Color.argb(255,255,255,255));
+            this.canvas.drawColor(Color.argb(255, 255, 255, 255));
             paintbrush.setColor(Color.WHITE);
 
 
@@ -219,23 +230,34 @@ public class GameEngine extends SurfaceView implements Runnable {
             drawLaneFour();
 
             //draw the dinosaur
-            this.canvas.drawBitmap(dinosour.getImage(),dinosour.getxPosition(),dinosour.getyPosition(),paintbrush);
+            this.canvas.drawBitmap(dinosour.getImage(), dinosour.getxPosition(), dinosour.getyPosition(), paintbrush);
+
+            //draw the candy
+            this.canvas.drawBitmap(candy.getImage(), candy.getxPosition(), candy.getyPosition(), paintbrush);
+
+            //draw the rainbow
+            this.canvas.drawBitmap(rainbow.getImage(), rainbow.getxPosition(), rainbow.getyPosition(), paintbrush);
+
+            //dra
+
+
             // DRAW THE PLAYER HITBOX
             // ------------------------
             // 1. change the paintbrush settings so we can see the hitbox
             paintbrush.setColor(Color.BLUE);
             paintbrush.setStyle(Paint.Style.STROKE);
             paintbrush.setStrokeWidth(5);
-            this.canvas.drawRect(dinosour.getHitbox(),paintbrush);
+            this.canvas.drawRect(dinosour.getHitbox(), paintbrush);
+            this.canvas.drawRect(candy.getHitbox(), paintbrush);
 
 
             //life label
             paintbrush.setTextSize(50);
-            canvas.drawText("lives:- " + this.lives, 1500, 50,paintbrush);
+            canvas.drawText("lives:- " + this.lives, 1500, 50, paintbrush);
 
             //score label
             paintbrush.setTextSize(50);
-            canvas.drawText("score:- " + this.score, 1200,50,paintbrush);
+            canvas.drawText("score:- " + this.score, 1200, 50, paintbrush);
 
             //----------------
             this.holder.unlockCanvasAndPost(canvas);
@@ -245,8 +267,7 @@ public class GameEngine extends SurfaceView implements Runnable {
     public void setFPS() {
         try {
             gameThread.sleep(120);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
 
         }
     }
@@ -259,16 +280,57 @@ public class GameEngine extends SurfaceView implements Runnable {
     String fingerAction = "";
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        int userAction = event.getActionMasked();
-        //@TODO: What should happen when person touches the screen?
-        if (userAction == MotionEvent.ACTION_DOWN) {
+    public boolean onTouchEvent(MotionEvent event)
+    {
+//        int middleOfScreen = (this.screenHeight / 2) - 100;
+//        int userAction = event.getActionMasked();
+//        //@TODO: What should happen when person touches the screen?
+//        if (userAction == MotionEvent.ACTION_DOWN) {
+//
+//
+//            if (event.getY() > middleOfScreen) {
+//                fingerAction = "lower";
+//                switch (dinoLane) {
+//                    case 1:
+//                        dinoLane = dinoLane + 2;
+//                        break;
+//                    case 2:
+//                        dinoLane++;
+//                        break;
+//                    case 3:
+//                        dinoLane++;
+//                        break;
+//                    case 4:
+//                        dinoLane--;
+//                        break;
+//                }
+//            } else if (event.getY() < middleOfScreen) {
+//                Log.d(TAG, "onTouchEvent: UP" + event.getY());
+//                fingerAction = "upper";
+//                switch (dinoLane) {
+//                    case 1:
+//                        dinoLane++;
+//                        break;
+//                    case 2:
+//                        dinoLane--;
+//                        break;
+//                    case 3:
+//                        dinoLane--;
+//                        break;
+//                    case 4:
+//                        dinoLane = dinoLane - 2;
+//                        break;
+//                }
+//
+//            } else if (userAction == MotionEvent.ACTION_UP) {
+////
+//                Log.d(TAG, "onTouchEvent: ACTION_UP");
+//                this.fingerAction = "up";
+//
+//            }
+//            return true;
+//        }
 
-        }
-        else if (userAction == MotionEvent.ACTION_UP) {
 
-        }
-
-        return true;
     }
 }
